1e7c33c044509b51c7c4f50c6819d449e65005e8
TatyanaErofeeva <pupchik@mail.ru>
2022-06-03 11:16:46 +0000

    В файл main.js добавляет функции для возврата целого числа и числа с плавающей точкой. Прописан путь к main.js в файле index.html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 /*Функция, возвращающая случайное целое число из переданного диапазона включительно*/
 
-function getRandomIntegerNumber(min, max){
-  return (min<0 || max<= min)?'underfind': Math.floor(Math.random() * (max - min + 1) + min);
-}
-getRandomIntegerNumber(1,3);
+const randomIntegerNumber = (min, max) => (min<0 || max<= min)? undefined: Math.floor(Math.random() * (max - min + 1) + min);
+randomIntegerNumber(1,3);
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 //Используется для генерации временных географических координат в следующем задании.
 
-function getRandomNotIntegerNumber(min, max, n){
-  return (min < 0 || max <= min)?'underfind': (Math.random() * (max - min + 1) + min).toFixed(n);
-}
-getRandomNotIntegerNumber(3, 5, 1);
+const randomNotIntegerNumber=(min, max, n) => (min < 0 || max <= min)? undefined: (Math.random() * (max - min + 1) + min).toFixed(n);
+randomNotIntegerNumber(3, 5, 1);
