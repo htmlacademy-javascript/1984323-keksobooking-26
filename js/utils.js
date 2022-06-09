8b4293283a1cdf7746961f15b06cf037c43372fe
TatyanaErofeeva <pupchik@mail.ru>
2022-06-09 12:09:03 +0000

    Код разделен на модули, которые подключены к main.js

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const randomIntegerNumber = (min, max) => (min<0 || max<= min)? undefined: Math.floor(Math.random() * (max - min + 1) + min);
+
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+//Используется для генерации временных географических координат в следующем задании.
+const randomNotIntegerNumber=(min, max, n) => (min < 0 || max <= min)? undefined: +(Math.random() * (max - min + 1) + min).toFixed(n);
+
+const getRandomArrayElement = (elements) => elements[randomIntegerNumber(0, elements.length - 1)];
+export {getRandomArrayElement,randomIntegerNumber,randomNotIntegerNumber};
