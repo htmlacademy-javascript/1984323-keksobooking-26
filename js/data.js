a8bb8f9219b357f51d6350c560a18fc42c23d9e4
TatyanaErofeeva <pupchik@mail.ru>
2022-06-21 16:05:26 +0000

    Создание DOM-элементов на основании тестовых данных. Модуль 7. Внесение корректировок на основании ревью

--- js/data.js
+++ js/data.js
@@ -40,58 +40,50 @@ const FEATURES_TYPES =[
   'conditioner'
 ];
 
-const arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10];
+const createUniqueNumbersArray = (count, min) => {
+  const numbers = [];
+  // собираем числа по порядку, начиная с минимально заданного
+  // и прибавляем количество необходимых нам чисел - получаем максимальное число
+  for (let i = min; i < count + min; i++) {
+    numbers.push(i);
+  }
 
-//Функция возвращающая значение элемента массива с ведущим 0
-function getNonRepeatableNumberOfPicture (min, max) {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = randomIntegerNumber(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      // eslint-disable-next-line prefer-template
-      // eslint-disable-next-line no-console
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = randomIntegerNumber(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue.toString().padStart(2,0);
-  };
-}
-const generatePhotoId = getNonRepeatableNumberOfPicture (1, 20);
+  return numbers
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+};
 
 // Определение полей для объекта offer
-function getNewArrayOfFeatures() {
+function getFeatures() {
   const maxLength = FEATURES_TYPES.length;
-  const newArray = [];
+  const features = [];
   const newArrayLength = randomIntegerNumber(1, maxLength);
-  while (newArray.length < newArrayLength) {
+  while (features.length < newArrayLength) {
     const indexOfEl = randomIntegerNumber(0, maxLength - 1);
     const el = FEATURES_TYPES[indexOfEl];
-    if (!newArray.includes(el)) {
-      newArray.push(el);
+    if (!features.includes(el)) {
+      features.push(el);
     }
   }
-  return newArray;
+  return features;
 }
 
 // photos, массив строк — массив случайной длины из значений:
-const getNewArrayOfPhotos = () => {
-  const newArray = [];
+const getPhotos = () => {
+  const photos = [];
   const newArrayLength = randomIntegerNumber(1, PHOTOS_TYPES.length);
   for (let i=0; i < newArrayLength; i++ ){
-    newArray.push(PHOTOS_TYPES[i]);
+    photos.push(PHOTOS_TYPES[i]);
   }
-  return newArray;
+  return photos;
 };
 
 //Функция по созданию типового объекта
-const createAnObject = () => {
+const createAnObject = (uniqueIds) => {
   const author = {
-    avatar: `img/avatars/user${generatePhotoId()}.png`
+    // берем из массива id первое значение и удаляем его
+    avatar: `img/avatars/user${uniqueIds.shift().toString().padStart(2, '0')}.png`
   };
 
   const location ={
@@ -108,9 +100,9 @@ const createAnObject = () => {
     guests: randomIntegerNumber(1,5),
     checkin: getRandomArrayElement(CHECKIN_VARIANTS),
     checkout: getRandomArrayElement(CHECKOUT_VARIANTS),
-    features: getNewArrayOfFeatures(),
+    features: getFeatures(),
     description:'Супер-мега-пуперистое помещение :)',
-    photos:getNewArrayOfPhotos(),
+    photos:getPhotos(),
   };
 
   return {
@@ -120,6 +112,16 @@ const createAnObject = () => {
   };
 };
 
-const createObjects = () => Array.from({length: SIMILAR_OBJECTS_COUNT}, createAnObject);
+let lastCreatedId = 1;
+const createObjects = () => {
+  // на каждый вызов создаем новый массив уникальных чисел
+  const uniqueIds = createUniqueNumbersArray(SIMILAR_OBJECTS_COUNT, lastCreatedId);
+  // запоминаем мксимальное число, которое будет у нас в массиве
+  lastCreatedId = lastCreatedId + SIMILAR_OBJECTS_COUNT;
+
+  // передаем полученный массив в функцию создания объекта
+  return Array.from({length: SIMILAR_OBJECTS_COUNT}, () => createAnObject(uniqueIds));
+};
 
 export {createObjects};
+
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 import { createObjects } from './data.js';
 
 const TYPES = {
@@ -28,25 +29,54 @@ const clonePhotoElement = (offer) => {
 
 const similarObjects = createObjects();
 const fragment = document.createDocumentFragment();
-const newArray =[];
+const objectElements =[];
+
+const featuresContainer = similarObjectTemplate.querySelector('.popup__features');
+const featuresList = featuresContainer.querySelectorAll('.popup__feature');
+
 similarObjects.forEach(({ offer, author} ) => {
   const objectElement = similarObjectTemplate.cloneNode(true);
+  if (!author.avatar) {
+    objectElement.querySelector('.popup__avatar').classList.add('hidden');
+  }
+  else {
+    objectElement.querySelector('.popup__avatar').src = author.avatar;
+  }
+  objectElement.querySelector('.popup__title').textContent = offer.title;
+  objectElement.querySelector('.popup__text--address').textContent = offer.address;
+  objectElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  objectElement.querySelector('.popup__type').textContent = TYPES[offer.type];
+  objectElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  objectElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
+  if (!offer.features || offer.features.length < 1|| undefined){
+    objectElement.querySelector('.popup__features').classList.add('hidden');
+  }
+  else{
+    const modifiers = offer.features.map((features) => 'popup__feature--' + features);
+    featuresList.forEach((featuresListItem) => {
+      const modifier = featuresListItem.classList[1];
+      if (!modifiers.includes(modifier)) {
+        featuresListItem.remove();
+      }
+    });
+  }
+
   if (!offer.description) {
     objectElement.querySelector('.popup__description').classList.add('hidden');
   }
   else{
-    objectElement.querySelector('.popup__avatar').src = author.avatar;
-    objectElement.querySelector('.popup__title').textContent = offer.title;
-    objectElement.querySelector('.popup__text--address').textContent = offer.address;
-    objectElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-    objectElement.querySelector('.popup__type').textContent = TYPES[offer.type];
-    objectElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-    objectElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-    objectElement.querySelector('.popup__features').textContent = offer.features;
     objectElement.querySelector('.popup__description').textContent = offer.description;
-    clonePhotoElement(offer);
-    newArray.push(objectElement);
   }
+  if (!offer.photos || offer.photos.length < 1|| undefined ){
+    objectElement.querySelector('.popup__photos').classList.add('hidden');
+  }
+  else{
+    clonePhotoElement(offer);
+  }
+
+  objectElements.push(objectElement);
+  //console.log(objectElement);
 });
-fragment.appendChild(newArray[0]);
+fragment.appendChild(objectElements[0]);
 firstTemporaryObject.appendChild(fragment);
