5bc717d1799c604117795bd8c8f7633351df0b7f
TatyanaErofeeva <pupchik@mail.ru>
2022-06-22 06:05:36 +0000

    Создание DOM-элементов на основании тестовых данных. Модуль 7. Внесение корректировок на основании ревью

--- js/popup.js
+++ js/popup.js
@@ -14,28 +14,14 @@ const similarObjectTemplate = document.querySelector('#card')
   .querySelector('.popup');
 
 const firstTemporaryObject = document.querySelector('#map-canvas');
-
-const photoElementParent = similarObjectTemplate.querySelector('div');
-const photoElementChild = similarObjectTemplate.querySelector('div').querySelector('img');
-
-const clonePhotoElement = (offer) => {
-  photoElementParent.innerHTML='';
-  for(let i = 0; i < offer.photos.length; i++){
-    const clonedElement = photoElementChild.cloneNode(false);
-    clonedElement.src = offer.photos[i];
-    photoElementParent.appendChild(clonedElement);
-  }
-};
-
 const similarObjects = createObjects();
 const fragment = document.createDocumentFragment();
 const objectElements =[];
 
-const featuresContainer = similarObjectTemplate.querySelector('.popup__features');
-const featuresList = featuresContainer.querySelectorAll('.popup__feature');
-
 similarObjects.forEach(({ offer, author} ) => {
   const objectElement = similarObjectTemplate.cloneNode(true);
+  const featuresContainer = objectElement.querySelector('.popup__features');
+  const featuresList = featuresContainer.querySelectorAll('.popup__feature');
   if (!author.avatar) {
     objectElement.querySelector('.popup__avatar').classList.add('hidden');
   }
@@ -49,7 +35,7 @@ similarObjects.forEach(({ offer, author} ) => {
   objectElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   objectElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
-  if (!offer.features || offer.features.length < 1|| undefined){
+  if (!offer.features || offer.features.length < 1){
     objectElement.querySelector('.popup__features').classList.add('hidden');
   }
   else{
@@ -68,11 +54,18 @@ similarObjects.forEach(({ offer, author} ) => {
   else{
     objectElement.querySelector('.popup__description').textContent = offer.description;
   }
-  if (!offer.photos || offer.photos.length < 1|| undefined ){
+  if (!offer.photos || offer.photos.length < 1){
     objectElement.querySelector('.popup__photos').classList.add('hidden');
   }
   else{
-    clonePhotoElement(offer);
+    const photoElementParent = objectElement.querySelector('div');
+    const photoElementChild = objectElement.querySelector('div').querySelector('img');
+    photoElementParent.innerHTML='';
+    for(let i = 0; i < offer.photos.length; i++){
+      const clonedElement = photoElementChild.cloneNode(false);
+      clonedElement.src = offer.photos[i];
+      photoElementParent.appendChild(clonedElement);
+    }
   }
 
   objectElements.push(objectElement);
